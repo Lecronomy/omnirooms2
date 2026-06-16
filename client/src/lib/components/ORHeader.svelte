00e93cf588d848716d201742e50f3e6ecffcc85b
Lecronomy <arnomuttilainenjalmari@hotmail.com>
2026-06-16 18:45:16 +0300

    client: header + room routing

---
+++ client/src/lib/components/ORHeader.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import { CircleUserIcon, MenuIcon } from '@lucide/svelte';
+	import { AppBar } from '@skeletonlabs/skeleton-svelte';
+</script>
+
+<AppBar>
+	<AppBar.Toolbar class="grid-cols-[1fr_2fr_1fr]">
+		<AppBar.Lead>
+			<button type="button" class="btn-icon btn-icon-lg hover:preset-tonal"><MenuIcon /></button>
+		</AppBar.Lead>
+		<AppBar.Headline class="flex justify-center">
+			<p class="text-2xl">Omnirooms</p>
+		</AppBar.Headline>
+		<AppBar.Trail class="justify-end">
+			<button type="button" class="btn-icon hover:preset-tonal"><CircleUserIcon class="size-6" /></button>
+		</AppBar.Trail>
+	</AppBar.Toolbar>
+</AppBar>
---
+++ client/src/routes/room/[roomName]/+page.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+    import { page } from '$app/state';
+</script>
+
+<p>The chatroom will be displayed here. Currennt room: {page.params.roomName}</p>
