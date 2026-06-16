b787374ad8cbd0caa126b6c96c43014ce7091da2
Lecronomy <arnomuttilainenjalmari@hotmail.com>
2026-06-16 19:22:33 +0300

    client: correct typo in chatroom display message

--- client/src/routes/room/[roomName]/+page.svelte
+++ client/src/routes/room/[roomName]/+page.svelte
@@ -2,4 +2,4 @@
     import { page } from '$app/state';
 </script>
 
-<p>The chatroom will be displayed here. Currennt room: {page.params.roomName}</p>
+<p>The chatroom will be displayed here. Current room: {page.params.roomName}</p>
