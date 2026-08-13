643b52526a136af9525e4a5784cdd63ce09c9d77
Lecronomy <arnomuttilainenjalmari@hotmail.com>
2026-08-13 16:51:18 +0300

    client: add basic account page

---
+++ client/src/routes/account/+page.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import { resolve } from '$app/paths';
+
+	import { useAuthState } from '$lib/states/authState.svelte';
+
+	const authState = useAuthState();
+</script>
+
+{#if authState.user}
+	<p>Logged in as: <b>{authState.user.username}</b></p>
+{:else}
+	<p>You are not logged in</p>
+	<p>
+		Please
+		<a class="anchor" href={resolve('/auth/login')}>log in</a>
+		or
+		<a class="anchor" href={resolve('/auth/register')}>register</a>
+		to access account settings!
+	</p>
+{/if}
