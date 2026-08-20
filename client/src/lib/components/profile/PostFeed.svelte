3d760214961aae80c47bd186dfcd702d570881df
Lecronomy <arnomuttilainenjalmari@hotmail.com>
2026-08-20 21:23:05 +0300

    client: add title to post feed

--- client/src/lib/components/profile/PostFeed.svelte
+++ client/src/lib/components/profile/PostFeed.svelte
@@ -2,19 +2,26 @@
 	import { Progress } from '@skeletonlabs/skeleton-svelte';
 
 	import { usePostsState } from '$lib/states/postsState.svelte';
+	import { useAuthState } from '$lib/states/authState.svelte';
 
 	let { username = null as string | null } = $props();
 
 	const postsState = usePostsState();
+	const authState = useAuthState();
 
 	$effect(() => {
 		if (username) {
 			postsState.fetchUserPosts(username);
 		} else {
+			username = authState.user?.username ?? 'Unknown user';
 			postsState.fetchMyPosts();
 		}
 	});
 </script>
+
+<h2 class="h2">
+	{username}'s posts
+</h2>
 
 {#if postsState.loading}
 	<Progress class="items-center w-fit" value={null}>
